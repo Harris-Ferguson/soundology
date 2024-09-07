435354eeb4a02543ed5b9b475fdd5809c1389fb3
Harris <harris.ferguson@usask.ca>
2024-09-06 19:32:40 -0600

    blegh

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,12 @@ int main( ){
 	ofGLESWindowSettings settings;
 	settings.glesVersion=2;
 #else
+	ofGLWindowSettings settings;
 	settings.setGLVersion(3,2);
 #endif
 
 	auto window = ofCreateWindow(settings);
 
-	ofGLFWWindowSettings settings;
     settings.windowMode = OF_FULLSCREEN;  // Set fullscreen mode
     settings.setSize(1920, 1080);         // Optionally, set your desired screen resolution
     ofCreateWindow(settings);
